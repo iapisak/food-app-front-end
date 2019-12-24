6bb0dbc7ddeadaf1e1180bae331e177e2810a867
potisanmetee <iapisak1981@gmail.com>
2019-12-23 21:09:52 -0800

    fix fetch on pull restaurant data

--- src/components/Container/RestaurantContainer.js
+++ src/components/Container/RestaurantContainer.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class RestaurantContainer extends Component {
+    state = {
+        restaurant_id: this.props.id,
+        restaurant_name: this.props.name,
+        result: [],
+    }
+
+
+    getRestaurantData = (e) => {
+        fetch(`https://us-restaurant-menus.p.rapidapi.com/${this.state.restaurant_id}/menuitem/`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
+                "x-rapidapi-key": "fe0837f0a2msh7cb5dab4b0d98c6p1b7249jsn44b6d3c3edbf"
+            }
+        })
+        .then(stream => stream.json())
+        .then(res => {
+            console.log(res)
+            this.setState({ result: res.result.data })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     render() {
         return (
+            <>
             <h3>{ this.props.name }</h3>
+            <h4>{ this.props.id }</h4>
+            </>
         )
     }
 }
