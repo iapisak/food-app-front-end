c5bc47158bf667d782fd97c9a9cf87ed85579656
potisanmetee <iapisak1981@gmail.com>
2019-12-23 23:18:12 -0800

    Add API for loading menu

--- src/components/Container/RestaurantContainer.js
+++ src/components/Container/RestaurantContainer.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class RestaurantContainer extends Component {
     state = {
         restaurant_id: this.props.id,
-        restaurant_name: this.props.name,
-        result: [],
+        name: this.props.name,
+        menu: [],
     }
 
+    createNewMenu = () => {
+        axios.put(`${process.env.REACT_APP_API_URL}/menu/create`, this.state)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
 
-    getRestaurantData = (e) => {
-        fetch(`https://us-restaurant-menus.p.rapidapi.com/${this.state.restaurant_id}/menuitem/`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
-                "x-rapidapi-key": "fe0837f0a2msh7cb5dab4b0d98c6p1b7249jsn44b6d3c3edbf"
-            }
-        })
-        .then(stream => stream.json())
+    componentDidMount () {
+        axios.get(`${process.env.REACT_APP_API_URL}/menu/${this.state.restaurant_id}`)
         .then(res => {
-            console.log(res)
-            this.setState({ result: res.result.data })
+            this.setState({ menu: res.data.data.menu })
         })
         .catch(err => {
-            console.log(err)
+            console.log('load')
+            fetch(`https://us-restaurant-menus.p.rapidapi.com/restaurant/${this.state.restaurant_id}/menuitems?page=1`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
+                    "x-rapidapi-key": "fe0837f0a2msh7cb5dab4b0d98c6p1b7249jsn44b6d3c3edbf"
+                }
+            })
+            .then(stream => stream.json())
+            .then(res => {
+                this.setState({ menu: res.result.data })
+                this.createNewMenu()
+            })
+            .catch(err => {
+                console.log(err)
+            })
         })
     }
 
